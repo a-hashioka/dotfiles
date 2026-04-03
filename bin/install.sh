8cb6c4038f4f101652275c94ff5f3b955f5456d2
a.hashioka <atsuro.3-hashioka.5@outlook.jp>
2026-04-03 23:22:39 +0900

    minor change

--- bin/install.sh
+++ bin/install.sh
@@ -53,7 +53,7 @@ install_packages() {
   rm -rf ~/.config/nvim/.git
 
   # install lazygit　(git ui)
-  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*")')
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
   curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
   tar xf lazygit.tar.gz lazygit
   sudo install lazygit /usr/local/bin
