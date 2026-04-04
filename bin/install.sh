c3df09a62c1381c4e6a8b7539f3fe5566d26e791
a.hashioka <atsuro.3-hashioka.5@outlook.jp>
2026-04-04 14:48:01 +0900

    minor change

--- bin/install.sh
+++ bin/install.sh
@@ -37,7 +37,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 # add current user to docker group to use docker without sudo
-# sudo usermod -aG docker "$USER"
+sudo usermod -aG docker "$USER"
 
 # install ghostty(terminal emulator)
 echo "[*] Installing ghostty..."
