62bc1935b00154afd2cc93b9df6f74fdc164f13b
a.hashioka <atsuro.3-hashioka.5@outlook.jp>
2026-04-18 23:41:52 +0900

    remove LazyVim

--- bin/install.sh
+++ bin/install.sh
@@ -78,10 +78,10 @@ git clone -q https://github.com/LazyVim/starter "$HOME/.config/nvim/"
 rm -rf "$HOME/.config/nvim/.git"
 
 # install lazygit　(git ui)echo "[*] Installing lazygit..."
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -fLo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit && sudo install lazygit /usr/local/bin
-rm -f lazygit.tar.gz lazygit
+# LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+# curl -fLo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+# tar xf lazygit.tar.gz lazygit && sudo install lazygit /usr/local/bin
+# rm -f lazygit.tar.gz lazygit
 
 # install docker from official repository for latest version
 echo "[*] Installing docker..."
